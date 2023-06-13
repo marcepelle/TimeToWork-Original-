0240a49ad2b214e02aa4531f38ce38703c45a889
marcepellerinal <marcepellerinal@gmail.com>
2023-06-14 00:17:57 +0200

    añadiendo la ip del despliegue de heroku

--- app/src/main/java/com/example/timetowork/utils/Apis.java
+++ app/src/main/java/com/example/timetowork/utils/Apis.java
@@ -1,18 +1,14 @@
 
 package com.example.timetowork.utils;
 
-import android.util.Log;
-
-import java.net.InetAddress;
-
 //clase que se encargará de instanciar un objeto Retrofit y definirá la ruta base(controller) de la API que queremos consultar
 public class Apis {
     //ruta del controller en la ip y puerto definidos
 
-    private static String localIp = "192.168.56.1";
-    public static final String URL_01="http://" + localIp + ":8080/usuarios/";
-    public static final String URL_02="http://" + localIp + ":8080/horarios/";
-    public static final String URL_03="http://" + localIp + ":8080/mensajes/";
+    private static String stringIp = "timetoworkproject.herokuapp.com";
+    public static final String URL_01="https://" + stringIp + "/usuarios/";
+    public static final String URL_02="https://" + stringIp + "/horarios/";
+    public static final String URL_03="https://" + stringIp + "/mensajes/";
 
     public static UsuarioService getUsuarioService(){
         return Cliente.getCliente(URL_01).create(UsuarioService.class); //Devolvemos un objeto retrofit definiendo la ruta base de la API (URL_01) e implementamos la interfaz (UsuarioService) con sus métodos abstractos para acceder a las rutas especificas
